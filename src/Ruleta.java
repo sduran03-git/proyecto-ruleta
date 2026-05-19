4ac5bf59013bc6dcced6bd4cbd54ec5d84bf4823
sduran03-git <s.duran03@ufromail.cl>
2026-05-19 18:36:34 -0400

    refactor: eliminar main de Ruleta

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -12,9 +12,6 @@ public class Ruleta {
     public static final int[] NUMEROS_ROJOS =
             {1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36};
 
-    public static void main(String[] args) {
-        menu();
-    }
 
     public static void menu() {
         Scanner in = new Scanner(System.in);
