1b1e93f55543228eaea5116481beeca933f492ad
sduran03-git <s.duran03@ufromail.cl>
2026-05-27 21:54:35 -0400

    feat: agregar clase Estadisticas creada desde VisualParadigm

---
+++ src/Modelo/Estadisticas.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+public class Estadisticas {
+
+	private List<Resultado> historial;
+
+	/**
+	 * 
+	 * @param historial
+	 */
+	public Estadisticas(List<Resultado> historial) {
+		// TODO - implement Estadisticas.Estadisticas
+		throw new UnsupportedOperationException();
+	}
+
+	public int getTotalJugadas() {
+		// TODO - implement Estadisticas.getTotalJugadas
+		throw new UnsupportedOperationException();
+	}
+
+	public int getVictorias() {
+		// TODO - implement Estadisticas.getVictorias
+		throw new UnsupportedOperationException();
+	}
+
+	public int getPorcentajeVictorias() {
+		// TODO - implement Estadisticas.getPorcentajeVictorias
+		throw new UnsupportedOperationException();
+	}
+
+	public int getRachaMaxima() {
+		// TODO - implement Estadisticas.getRachaMaxima
+		throw new UnsupportedOperationException();
+	}
+
+	public TipoApuesta getTipoMasJugado() {
+		// TODO - implement Estadisticas.getTipoMasJugado
+		throw new UnsupportedOperationException();
+	}
+
+}
