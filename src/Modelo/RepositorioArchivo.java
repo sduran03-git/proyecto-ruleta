33c20877fcb46f2b72f70dccc64c2b56633b5799
sduran03-git <s.duran03@ufromail.cl>
2026-06-02 19:20:52 -0400

    feat: agregar esqueleto de RepositorioArchivo generado por ingeniería directa

---
+++ src/Modelo/RepositorioArchivo.java
@@ -0,0 +1,19 @@
+package Modelo;
+
+public class RepositorioArchivo implements IRepositorioResultados {
+
+	/**
+	 * 
+	 * @param resultado
+	 */
+	public void agregar(Resultado resultado) {
+		// TODO - implement RepositorioArchivo.agregar
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Resultado> obtenerTodos() {
+		// TODO - implement RepositorioArchivo.obtenerTodos
+		throw new UnsupportedOperationException();
+	}
+
+}
