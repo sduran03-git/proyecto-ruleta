494b8fdd6d898a41c2a36a0d31975cdb33b5b77e
sduran03-git <s.duran03@ufromail.cl>
2026-06-02 19:20:39 -0400

    feat: agregar esqueleto de RepositorioEnMemoria generado por ingeniería directa

---
+++ src/Modelo/RepositorioEnMemoria.java
@@ -0,0 +1,19 @@
+package Modelo;
+
+public class RepositorioEnMemoria implements IRepositorioResultados {
+
+	/**
+	 * 
+	 * @param resultado
+	 */
+	public void agregar(Resultado resultado) {
+		// TODO - implement RepositorioEnMemoria.agregar
+		throw new UnsupportedOperationException();
+	}
+
+	public List<Resultado> obtenerTodos() {
+		// TODO - implement RepositorioEnMemoria.obtenerTodos
+		throw new UnsupportedOperationException();
+	}
+
+}
